e68a223781fc441b670d0f95db58b702a4193cfa
Adriano Systax <adriano@systax.com.br>
2019-07-30 15:51:04 -0300

    Novo ajuste sys

--- ProjGit01/Program.cs
+++ ProjGit01/Program.cs
@@ -9,6 +9,7 @@ namespace ProjGit01
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Ok, vamos testar!");
+            Console.WriteLine("Funcionando 1, 2, 3!");
 
             Console.WriteLine("Novo texto 01");
         }
